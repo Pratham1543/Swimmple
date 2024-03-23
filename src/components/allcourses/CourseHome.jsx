7b3fb2f5d41d4f1c602a508e02861b90f8289cc1
Pratham <prathampatel.it20@scet.ac.in>
2024-03-23 14:03:56 +0530

    gotop button in course home

--- src/components/allcourses/CourseHome.jsx
+++ src/components/allcourses/CourseHome.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import Back from "../common/back/Back"
 import CoursesCard from "./CoursesCard"
-
+import GoToTopButton from "../home/GoToTopButton"
 const CourseHome = () => {
   return (
     <>
